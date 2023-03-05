7437bc7eeca571f9855b1d45859a17dddcdc6645
Mobik <sdzebluk@gmail.com>
2023-03-05 11:52:14 +0200

    target framerate setup

---
+++ Assets/CodeBase/ApplicationSetup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BlackBall
+{
+    public class ApplicationSetup : MonoBehaviour
+    {
+        [SerializeField] private int _targetFrameRate = 90;
+        
+        private void Awake()
+        {
+            Application.targetFrameRate = _targetFrameRate;
+        }
+    }
+}
--- Assets/CodeBase/FieldMover.cs
+++ Assets/CodeBase/FieldMover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using BlackBall.Platforms;
 using UnityEngine;
 
@@ -7,7 +8,9 @@ namespace BlackBall
     public class FieldMover : MonoBehaviour
     {
         [SerializeField] private PlatformsSpawner _platformsSpawner = null!;
-        [SerializeField] private float _fieldSpeed = 0.5f;
+        [SerializeField] private float _fieldSpeed = 0.3f;
+        // 100c = +0.1f
+        [SerializeField] private float _speedMultiplier = 0.00002f;
         [SerializeField] private float _scoreFactor = 1.5f;
         
         private void Start()
@@ -15,6 +18,11 @@ namespace BlackBall
             _platformsSpawner.PlatformSpawned += OnPlatformSpawned;
 
             StartCoroutine(GameplayCoroutine());
+        }
+
+        private void FixedUpdate()
+        {
+            _fieldSpeed += _speedMultiplier;
         }
 
         private void OnPlatformSpawned(PlatformBase platform)
